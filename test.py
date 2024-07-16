571cc63c276e837938a72be724f2b1c505f72520
raypan891108 <raypan891108@gmail.com>
2024-07-16 21:43:33 +0800

    Oklab

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # 导入 3D 绘图工具
-from colour.models import Lab_to_XYZ, XYZ_to_sRGB, sRGB_to_XYZ, XYZ_to_Oklab
+from colour.models import Lab_to_XYZ, XYZ_to_sRGB, XYZ_to_Oklab
 
 def sRGB_to_linear(srgb):
     threshold = 0.04045
@@ -24,24 +24,24 @@ def linear_to_srgb(linear_rgb):
 lab_values = np.array([
     [18.3, 13.2, -12.2],
     [39.1, 28.3, 16],
+    [44.1, 23.9, 18.6],
+    [61.2, -4.8, 42],
     [38.5, -19, 5.6],
     [26.9, 8.9, -22.9],
-    [61.2, -4.8, 42],
-    [44.1, 23.9, 18.6],
     [66.7, -1.3, 1.3]
 ])
 
 # 转换到 sRGB 并转换到线性 RGB
 XYZ_values = Lab_to_XYZ(lab_values)
 test = XYZ_values
-test2 = XYZ_to_Oklab(XYZ_values)
-print(test2)
+
 # 定义多个独立的 test[1] 组
 test_1_sets = np.array([
    test[1], test[2], test[3], test[4], test[5]
 ])
 
-
+# 创建一个空列表来存储插值后的数据点
+interpolated_points = []
 
 # 创建一个包含初始点的数组，并插入每个 test[1] 组进行插值
 def interpolate_points(data_array, num_points):
@@ -56,17 +56,19 @@ def interpolate_points(data_array, num_points):
     return data_array
 
 # 插值并绘制结果
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111, projection='3d')
 
+# 绘制 lab_values 的插值结果
 for test_1 in test_1_sets:
     data_array = np.vstack((test[0], test_1, test[6]))
-    data_array = interpolate_points(data_array, 1000)
+    data_array = interpolate_points(data_array, 30)
+    
+    # 将插值后的数据点添加到列表中
+    interpolated_points.append(data_array)
 
     result_color = XYZ_to_sRGB(data_array)
-   
     result = XYZ_to_Oklab(data_array)
-    
 
     # 提取分量
     L = result[:, 0]
@@ -75,15 +77,42 @@ for test_1 in test_1_sets:
 
     # 绘制散点图，点的颜色为自身的归一化值
     ax.scatter(a, b, L, c=result_color, marker='o')
+for i in range(len(interpolated_points[0])):
+    data_array = np.vstack((interpolated_points[0][i], interpolated_points[1][i], interpolated_points[2][i],
+                            interpolated_points[3][i], interpolated_points[4][i], interpolated_points[0][i]))
+    data_array = interpolate_points(data_array, 30)
 
+     # 将插值后的数据点添加到列表中
+    interpolated_points.append(data_array)
+
+    result_color = XYZ_to_sRGB(data_array)
+    result = XYZ_to_Oklab(data_array)
+
+    # 提取分量
+    L = result[:, 0]
+    a = result[:, 1]
+    b = result[:, 2]
+
+    # 绘制散点图，点的颜色为自身的归一化值
+    ax.scatter(a, b, L, c=result_color, marker='o')
+    
+    
+L_zero_line = np.linspace(0, 1, 100)  # 假设 L 的范围是从 0 到 1
+ax.plot([0] * len(L_zero_line), [0] * len(L_zero_line), L_zero_line, color='red', linestyle='--', label='a=0, b=0')
+
+# 设置图形参数
 ax.set_xlabel('a*')
 ax.set_ylabel('b*')
 ax.set_zlabel('L*')
-
 ax.set_xlim(-0.4, 0.4)
 ax.set_ylim(-0.4, 0.4)
 ax.set_zlim(0, 1)
-
 ax.set_title('Interpolated Points in 3D Space')
 
 plt.show()
+
+# 打印所有插值后的数据点
+for idx, points in enumerate(interpolated_points):
+    print(f"Set {idx + 1}:")
+    print(points)
+    print("=" * 20)
